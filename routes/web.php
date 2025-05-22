e340919db0d30007baf134ee8637002b3188634f
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2025-05-21 22:14:47 -0300

    🚧 rotas web

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
+Route::get('gradicos', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('graficos');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
